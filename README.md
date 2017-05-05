0905b73d00df7a19aa4dbc92fd986ef1ba02ce56
Kyle Ramirez <kyletramirez@me.com>
2017-05-04 22:16:15 -0500

    Updating from Redux Flute & bye bye Redux Flute

--- README.md
+++ README.md
@@ -3,41 +3,159 @@
 ### What does it do?
 Reactive Record is a front-end-only Object Data-store Mapping (ODM) implementation that lets you interact with RESTful APIs. By defining models on the front end and integrating closely with the popular state container, Redux, Reactive Record offers a Ruby-on-Rails-esque, Active Record-ey syntax. Think Active Record for JavaScript. Reactive Record is agnostic to back-end architecture. It's built for APIs which respond to GET/POST/POST/DELETE requests for resources identified by predefined keys, so Rails, Express, Sinatra, CouchDB UNAMEIT! Reactive Record allows you to write syntax like this:
 
-```js
-const userAddress = new Address
+    const userAddress = new Address
 
-userAddress.address1 = "1100 Congress Ave"
-userAddress.city = "Austin"
-userAddress.state = "TX"
+    userAddress.address1 = "1100 Congress Ave"
+    userAddress.city = "Austin"
+    userAddress.state = "TX"
 
-userAddress.save()
-/* 
-  REQUEST:
-    Method: POST 
-    URL: /api/addresses
-    Data: {
-      "address1": "1100 Congress Ave",
-      "city": "Austin",
-      "state": "TX"
-    }
-  RESPONSE:
-    Status: 201 Created
-    Body: {
-      "_id": "583132c8edc3b79a853b8d69",
-      "createdAt": "2016-11-20T05:21:12.988Z",
-      "updatedAt": "2016-11-20T05:21:12.988Z",
-      "userId": "580432279153ea2679095acd",
-      "address1": "1100 Congress Ave",
-      "city": "Austin",
-      "state": "TX"
-    }
-*/
-userAddress.id
-// Returns 583132c8edc3b79a853b8d69
-userAddress.destroy()
-// Does what you would expect (A DELETE request to the same resource)
-```
+    userAddress.save()
+    /* 
+      REQUEST:
+        Method: POST 
+        URL: /api/addresses
+        Data: {
+          "address1": "1100 Congress Ave",
+          "city": "Austin",
+          "state": "TX"
+        }
+      RESPONSE:
+        Status: 201 Created
+        Body: {
+          "_id": "583132c8edc3b79a853b8d69",
+          "createdAt": "2016-11-20T05:21:12.988Z",
+          "updatedAt": "2016-11-20T05:21:12.988Z",
+          "userId": "580432279153ea2679095acd",
+          "address1": "1100 Congress Ave",
+          "city": "Austin",
+          "state": "TX"
+        }
+    */
+    userAddress.id
+    // Returns 583132c8edc3b79a853b8d69
+    userAddress.destroy()
+    // Does what you would expect (A DELETE request to the same resource)
+
 
 ### Installation
+    npm install --save reactiverecord
+    
+### Prerequisites
+This library was made to solve problems in my current project stacks (insert buzz words: React/Redux/Webpack/ES6). The only real assumptions are:
 
-    npm install --save-dev reactiverecord
+ - You are using NPM as a package manager
+ - You are using Redux
+
+The nice-to-haves are:
+
+ - React
+ - React Redux
+
+I'm open to suggestion on making this library more widely supported.
+
+# Minimum Setup
+
+### In your app
+    import ReactiveRecord from "reactiverecord";
+    const Story = ReactiveRecord.model("Story"),
+
+    const newStory = new Story;
+    newStory.save().then(savedRecord=>(this.setState({ ...savedRecord }));
+    // Also works ...
+    Story.create({ title: "A working title", body: "Once upon a time..."})  // Makes an API request
+      .then(savedRecord=>(this.setState({ ...savedRecord }));  // Returns a promise ... as do the following methods
+    newStory.updateAttribute("title", "A working title") // Makes an API request
+    newStory.updateAttributes({ title: "A working title", body: "Once upon a time..."})  // Makes an API request
+
+    newStory.destroy() // Makes an API request
+
+    Story.all() // Makes an API request to the model's index, returns array of records
+    Story.find("583132c8edc3b79a853b8d69") // Makes an API request to this resource, returns single record
+
+    // Passing extra URL query parameters
+    Story.all({month:"05", year:2017}) // Makes an API request to /stories?month=05&year=2017
+    Story.all("?month=05&year=2017") // Also works
+    Story.find("first-post", { include_comments: true }) // Makes an API request to /stories/first-post?include_comments=true
+    Story.find("first-post", "?include_comments=true") // Also works
+    // Use cases for extra query params
+    Search.all({q: "Am I being detained?"}) // Generates /search?q=Am%20I%20being%20detained%3F
+
+### Defining Models
+    // In a file like /models/Story.js
+    import ReactiveRecord, { Model } from "reactiverecord";
+    class Story extends Model {
+      static schema = {
+        title: String,
+        body: String,
+        isActive: Boolean,
+        userId: String,
+        _timestamps: true
+      }
+    }
+    export default ReactiveRecord.model(Story);
+
+
+### In your reducers setup
+
+    // In a file like /reducers.js
+    import { combineReducers } from "redux"
+    import ReactiveRecord, { reducer as models } from "reactiverecord"
+    ReactiveRecord.setAPI({ prefix: "/api" })
+    import "models"
+    export default combineReducers({
+      models,
+      // your other reducers
+    });
+
+### In your store setup
+
+    // In a file like /store.js
+    import { createStore, applyMiddleware, compose } from "redux";
+    import reducer from "./reducers";
+    import { middleware as reactiveRecordMiddleware } from "reactiverecord";
+    export default createStore(reducer, compose( applyMiddleware(reactiveRecordMiddleware /* , ...your other middlewares*/)));
+
+
+### Minification
+If you are running your build through something like Uglify, or part of a Webpack build, you'll need to exclude the class names of your models. Uglify supports excluding certain keywords from the minification/mangle process. Below is an example configuration for Webpack, in the plugins section of your webpack config.
+
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: {
+        except: ["BankAccount", "CreditCard","User"]
+      }
+    })
+
+# Coming Soon!
+
+ - **Model associations** syntax a-la `userAddress.user` with declarations like:
+
+        const User = ReactiveRecord.model("User")
+        class Address extends Model {
+          static associations = [
+            {belongsTo: User}
+          ]
+        }
+ 
+- **Validations**, with the ability to exclude local validation like `userAddress.save({validate: false})`, with declarations a-la Mongoose like:
+
+        class Address extends Model {
+          static schema = {
+            address1: {
+              type: String,
+              required: [true, "A street address is required."]
+            }
+            zip: {
+              type: String,
+              length: {
+                min: 5,
+                message: "ZIP codes must be at least 5 numbers."
+              }
+            }
+           // ... other schema
+          }
+        }
+
+ Also planned is the creation of a ReactiveRecord validations API, with the ability to include local and remote validations.
+ - **Scopes** and default scope syntax like `orders.completed`
+ - **Order** and default ordering `cards.orderBy("price", "ASC")`
+ - **Custom schema types** such as automatic conversions to U.S. dollar
---
+++ src/utils.js
@@ -0,0 +1,259 @@
+import Sugar from "./sugar"
+import { actionMatch, singleRecordProps, recordProps, versioningProps, restVerbs } from "./constants"
+
+export function isEmptyObject(obj){
+  for (let name in obj) {
+    return false;
+  }
+  return true;
+}
+
+export function generateID() {
+  function s4(){
+    return Math
+      .floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return `_${s4()+s4()+s4()}${s4()+s4()+s4()}`;
+}
+
+export function pruneDeep(obj){
+  return function prune(current){
+    for (let key in current) {
+      if (current.hasOwnProperty(key)) {
+        if (current[key] instanceof Array){
+          current[key] = pruneArray(current[key])
+        }
+
+        let value = current[key];
+        if (typeof value === "undefined" || value == null ||
+            (value != null && typeof value === "object" && isEmptyObject(prune(value))) ||
+            (value instanceof Array && value.length === 0)
+           ) {
+          delete current[key]
+        }
+      }
+    }
+    return current
+  }(Object.assign({}, obj))
+}
+
+export function pruneArray(arr) {
+  const newArray = new Array();
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] != null && typeof arr[i] === "object")
+      arr[i] = pruneDeep(arr[i])
+
+    if (typeof arr[i] === "undefined" || arr[i] === null) continue;
+    if (typeof arr[i] === "object" && isEmptyObject(arr[i])) continue;
+    if (typeof arr[i] === "number" && isNaN(arr[i])) continue;
+
+    newArray.push(arr[i]);
+  }
+  return newArray;
+}
+
+export function regexIndexOf(regex, string, startpos=0){
+  var indexOf = string.substring(startpos).search(regex);
+  return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
+}
+
+export function checkResponseStatus(response){
+  const {status} = response,
+        error = new Error;
+  // If no error, great, return the response.
+  if (status >= 200 && status < 300)
+    return response
+
+  // Begin parsing this error
+  error.status = status
+  error.response = response
+  throw error
+}
+
+export function routePermitted({ only, except }, method) {
+  if ((only instanceof Array && only.indexOf(method) === -1) || (typeof only === "string" && only !== method))
+    return false
+  if ((except instanceof Array && except.indexOf(method) !== -1) || (typeof except === "string" && except === method))
+    return false
+  return true
+}
+
+export function generateRoute(name, method, apiDelimiter, prefix, index=false, isSingleton) {
+  /*
+  GET    /stories      #index
+  GET    /stories/:id  #show
+  POST   /stories      #create
+  PUT    /stories/:id  #update
+  DELETE /stories/:id  #destroy
+  */
+  const delimiter = delimiterType(apiDelimiter),
+        modelInflection = isSingleton ? name : Sugar.String.pluralize(name),
+        modelWithDelimiter = `/${Sugar.String[delimiter](modelInflection)}`,
+        id = method === "POST" || index ? "" : "/:id";
+  return `${prefix}${modelWithDelimiter}${id}`
+}
+
+export function interpolateRoute(route, record) {
+  return route.replace(/:([^\/\?]*)/g, (match, capture)=>(
+    record.hasOwnProperty(capture) && record[capture] ? record[capture] : match
+  ))
+}
+
+export function delimiterType(delim="") {
+  if (delim.match(/^(underscores?|_)$/)) return "underscore"
+  return "dasherize"
+}
+
+export function setReadOnlyProps(params, _timestamps, modelName, _obj, reactiveRecord){
+  const { id, _id } = params;
+  // Establish the ID, also _id
+  if (id || _id) _obj.record.id = id || _id;
+
+  Object.defineProperty(_obj, "id", {
+    enumerable: true,
+    get: ()=>(_obj.record.id || null),
+    // ID can only be set on instantiation, otherwise it stays undefined
+    set: ()=>{throw new TypeError(`#<${modelName}> property \`id\` cannot be redefined.`)}
+  })
+
+  const defineVersion = function(newValue){
+    delete this._version;
+    Object.defineProperty(this, "_version", {
+      enumerable: false,
+      configurable: true,
+      get:()=>(newValue || 0),
+      set:defineVersion.bind(this)
+    })
+  }
+  defineVersion.call(_obj, params._version)
+
+  Object.defineProperty(_obj, "_request", {
+    enumerable: false,
+    value:{
+      version: 0,
+      status: null,
+      body: null,
+      ...params._request
+    }
+  })
+  Object.defineProperty(_obj._request, "clear", {
+    enumerable: false,
+    value: ()=>{
+      for (let property in _obj._request) {
+        if (property != "clear" && property != "version")
+          _obj._request[property] = null
+        if (property == "version")
+          _obj._request[property] = 0
+      }
+    }
+  })
+  Object.defineProperty(_obj, "errors", {
+    enumerable: false,
+    value:{...params.errors}
+  })
+  Object.defineProperty(_obj.errors, "clear", {
+    enumerable: false,
+    value: ()=>{for (let property in _obj.errors) if (property != "clear") delete _obj.errors[property]}
+  })
+
+  if (_timestamps) {
+    // Timestamps aren't something we're going to ever
+    // update on the record, so let's separate it early on
+    Object.defineProperty(_obj, "timestamps", {
+      enumerable: false,
+      value: {}
+    })
+    // Handle the createdAt
+    // Let it be undefined if nothing was given
+    _obj.timestamps.createdAt = params.created_at || params.createdAt || null
+    Object.defineProperty(_obj, "createdAt", {
+      enumerable: true,
+      get: ()=>(_obj.timestamps.createdAt ? new Date(_obj.timestamps.createdAt) : null),
+      // createdAt can only be set on instantiation, otherwise it stays undefined
+      set: ()=>{throw new TypeError(`#<${modelName}> property \`createdAt\` cannot be redefined.`)}
+    })
+
+    // Handle the updatedAt
+    // Let it be undefined if nothing was given
+    _obj.timestamps.updatedAt = params.updated_at || params.updatedAt || null
+    Object.defineProperty(_obj, "updatedAt", {
+      enumerable: true,
+      get: ()=>(_obj.timestamps.updatedAt ? new Date(_obj.timestamps.updatedAt) : null),
+      // updatedAt can only be set on instantiation, otherwise it stays undefined
+      set: ()=>{throw new TypeError(`#<${modelName}> property \`updatedAt\` cannot be redefined.`)}
+    })
+  }
+}
+
+export function setWriteableProps(params, schema, _obj, reactiveRecord){
+  for (let prop in schema){
+    const initialValue = params.hasOwnProperty(prop) ? params[prop] : null;
+    _obj.record[prop] = initialValue
+
+    let get = ()=>(_obj.record[prop])
+    // @TODO: The set function should dispatch an action that something was set, which
+    // would be used to increase the version number, and thus invalidate errors
+    let set = (newValue)=>{
+      reactiveRecord.setModel(_obj) //, prop, newValue)
+      return _obj.record[prop] = newValue
+    }
+
+    if (schema[prop].name === "Array")
+      get = ()=> (_obj.record[prop] || [])
+    if (schema[prop].name === "Date")
+      get = ()=> (_obj.record[prop] === null ? null : new Date(_obj.record[prop]))
+    if (schema[prop].name === "Number")
+      get = ()=> (_obj.record[prop] === null ? null : Number(_obj.record[prop]))
+    if (schema[prop].name === "Boolean") {
+      if (_obj.record[prop] !== null) {
+        _obj.record[prop] = initialValue === "false" ? false : Boolean(initialValue)
+      }
+      set = (newValue)=>{
+        reactiveRecord.setModel(_obj) //, prop, newValue)
+        return _obj.record[prop] = newValue === "false" ? false : Boolean(newValue)
+      }
+    }
+
+    Object.defineProperty(_obj, prop, { get, set, enumerable: true })
+  }
+}
+
+export function mergeRecordsIntoCache(cache, records, keyStr, model) {
+  // Get the records ready for the cache
+  const recordsForCache = records.map(record=>({...singleRecordProps, record: createThisRecord(model, record)}));
+        // Remove anything in the cache that matches keys in the records
+  const filteredCache = cache.filter(cacheItem=>{
+          let match = false;
+          recordsForCache.map(recordsItem=>{
+            if (recordsItem.record[keyStr] == cacheItem.record[keyStr]) match = true
+          })
+          return !match
+        })
+  // Finally, merge the new records and the filtered records
+  return [].concat(filteredCache, recordsForCache);
+}
+
+export function createThisRecord(model, rawRecord) {
+  const newInstance = new model(rawRecord)
+  return { ...newInstance.record, ...newInstance.timestamps }
+}
+
+export function tmpRecordProps(){
+  return({
+    id: generateID(),
+    ...versioningProps,
+    ...recordProps,
+    creating: false
+  });
+};
+
+export function objToQueryString(obj) {
+  return Object.keys(obj).reduce( (final, current) => {
+    const prefix = final.length? "&" : "?",
+          key = encodeURIComponent(current),
+          value = encodeURIComponent(obj[current]);
+    return `${final}${prefix}${key}=${value}`;
+  }, "" )
+}
